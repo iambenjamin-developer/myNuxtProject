9f8ed7e7ad7fb1d7d74dfaca94d07d1e5452b4da
Benjamin Correa <iambenjamin.developer@gmail.com>
2025-04-24 11:27:25 -0300

    ruteo

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,14 @@
+import { NuxtLink } from '../.nuxt/components';
 <template>
   <div>
+    <div>
+      <NuxtLink to="/">Home</NuxtLink>
+      <br>
+      <NuxtLink to="/blog">Blog</NuxtLink>
+      <br>
+      <NuxtLink to="/about">About</NuxtLink>
+      <br>
+    </div>
     <NuxtPage />
   </div>
 </template>
---
+++ src/pages/blog/[id].vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h2>{{ id }}</h2>
+    </div>
+</template>
+
+<script setup lang="ts">
+// ✅ Correcto: useRoute() dentro de <script setup>
+const route = useRoute()
+const id = route.params.id  // Accede al parámetro dinámico
+</script>
