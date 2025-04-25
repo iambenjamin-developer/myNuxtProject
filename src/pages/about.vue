10d831cee93dc407437ecf1f4d9be0266cad035d
Benjamin Correa <iambenjamin.developer@gmail.com>
2025-04-25 08:19:54 -0300

    tipos

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -2,15 +2,24 @@
     <div>
         <h1>About page</h1>
         <h2>{{ user.name.first }} {{ user.name.last }}</h2>
+        <h3>{{ user.login.password }}</h3>
+        <i>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis facilis, amet nesciunt autem sed numquam
+            eveniet nostrum dolore tempore error doloribus saepe quidem ducimus corrupti. Ducimus incidunt nostrum
+            tenetur corrupti.</i>
     </div>
 </template>
 
 <script setup lang="ts">
-import type { User } from '~~/interfaces/user';
+import type { Result, User } from '~~/interfaces/user';
 
-const { data, pending, error, refresh } = await useFetch('https://randomuser.me/api/')
-
-const user = computed(() => {
+const { data, error, refresh } = await useFetch(`https://randomuser.me/api/`, {
+    method: 'get',
+    query: {
+        gender: 'female',
+        password: 'upper,lower,1-16'
+    }
+})
+const user = computed<Result>(() => {
     const d = data.value as User
     return d.results[0]
 });
