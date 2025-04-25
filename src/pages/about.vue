d7aace934e59e46633305c968c09b3d97b74610d
Benjamin Correa <iambenjamin.developer@gmail.com>
2025-04-25 08:06:55 -0300

    fetch

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,19 +1,21 @@
 <template>
     <div>
         <h1>About page</h1>
+        <h2>{{ user.name.first }} {{ user.name.last }}</h2>
     </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
+import type { User } from '~~/interfaces/user';
 
-export default defineComponent({
-    setup() {
+const { data, pending, error, refresh } = await useFetch('https://randomuser.me/api/')
 
+const user = computed(() => {
+    const d = data.value as User
+    return d.results[0]
+});
 
-        return {}
-    }
-})
 </script>
+
 
 <style scoped></style>
